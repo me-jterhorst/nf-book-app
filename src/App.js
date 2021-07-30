e476ddecbed35141e737b7ce29a396d57e168d5e
Jakob ter Horst <me@jterhorst.de>
2021-07-30 15:08:51 +0200

    Add react header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 /**
  *
@@ -26,7 +26,11 @@ import "./App.css";
  */
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header title={"Titel"} />
+    </div>
+  );
 }
 
 export default App;
